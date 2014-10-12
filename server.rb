77ce480e519652a55e0e3f083ad69a4e1d326f7d
Heather Crowley <heathercrowley@Heathers-MacBook-Air.local>
2014-10-12 21:45:18 +0100

    thread pool

--- server.rb
+++ server.rb
@@ -1,40 +1,56 @@
-require 'socket'               # Get sockets from stdlib
+require 'socket'                 
+require 'thread'
 
-portnum = ARGV[0] #take port number as argument from command line
-server = TCPServer.open(portnum)  # Socket to listen on port 2000
-
-
-while true 
-  loop {                       # Servers run forever 
-        
-      Thread.start(server.accept) do |client|
-      #client = server.accept     # Wait for a client to connect
-      line = client.gets
-    
-      if line == "HELO\n"
-        puts "special message received"
-        client.puts "HELO text\nIP:[ip address]\nPort:[#{portnum}]\nStudentID:[10303365]"
-      
-      elsif line == "KILL_SERVICE\n"
-        puts "close message received"
-        puts "Server closing"
-        server.close
-        break                    #break out of loop
-      
-      else
-        puts "unknown message"
-      end
-        client.close               # Disconnect from the client
-        
-      end #end of do client
-      
-  }
-  
-  break                        #break out of while loop
-
-
+class Server                                                
+  attr_accessor :socket, :IsOpen                           
+  def initialize()                  
+   @socket = TCPServer.open(PortNumber)
+   @IsOpen = true
+  end
 end
 
 
+def serveClient(client, server, portnum)
+  line = client.gets
+  if line.include?("HELO\n")
+    puts "special message received and reply sent"
+    client.puts "HELO text\nIP:[#{IPAddress}]\nPort:[#{portnum}]\nStudentID:[10303365]"
+  elsif line == "KILL_SERVICE\n"
+    puts "Server closing"
+    server.socket.close              #this needs to be fixed- atm it just crashes
+    server.IsOpen = false
+  else
+    puts "unknown message"    #default for other messages
+  end
+  client.close                # Disconnect from client
+end
 
-#close breaks
+
+#create work queue and server
+IPAddress = Socket::getaddrinfo(Socket.gethostname,"echo",Socket::AF_INET)[0][3]
+PortNumber= ARGV[0]           #take port number as argument from command line
+server = Server.new()
+work_q = Queue.new
+
+
+# 5 threads serve the clients
+workers = (0...5).map do
+  Thread.new do
+    begin   
+      while server.IsOpen
+       if work_q.length > 0
+          client = work_q.pop                
+          serveClient client, server, PortNumber
+         end                                        
+     end
+    rescue ThreadError
+    end
+  end
+end; 
+
+
+#main loop continues to accept clients and push them onto the queue
+while server.IsOpen                                                                          
+  work_q.push(server.socket.accept)  
+end
+workers.map(&:join);    #stop all threads when they have completed serving
